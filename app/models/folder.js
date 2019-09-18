494a2f39fff7a78388d5ec379ce6a348aff323a1
amir <amirabuhasan@gmail.com>
2019-09-18 18:11:50 +0800

    updates

--- app/models/folder.js
+++ app/models/folder.js
@@ -1,8 +1,10 @@
-import DS from 'ember-data';
+import DS from "ember-data";
 
 const { hasMany, Model, attr } = DS;
 
 export default Model.extend({
-  name: attr('string'),
-  files: hasMany('file')
+  name: attr("string"),
+  files: hasMany("file"),
+  parent: DS.belongsTo("folder", { inverse: "folders" }),
+  folders: DS.hasMany("folder", { inverse: "parent" })
 });
